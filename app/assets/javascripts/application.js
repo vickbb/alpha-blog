53269d27c8d7b2e4556f9680f4a1d6640267e3ea
Victor Barreiros <victor_barreiros@hotmail.com>
2017-10-01 17:45:19 -0300

    Estilizando a listagem de artigos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require jquery
 //= require bootstrap
-//= require rails-ujs
 //= require bootstrap-sprockets
+//= require jquery
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
